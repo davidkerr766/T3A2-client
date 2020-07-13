208dfc625b2e2eccd72d89df2e2113dbf096f766
davidkerr766 <david.kerr766@outlook.com>
2020-07-14 09:21:15 +1000

    react-router-dom added and basic nav set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
+import About from "./components/About";
+import Recipes from "./components/Recipes";
+import Blog from "./components/Blog";
 
 const App = () => {
   return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+    <BrowserRouter>
+      <nav>
+        <Link to="/">About</Link>
+        <Link to="/recipes">Recipes</Link>
+        <Link to="/blog">Blog</Link>
+      </nav>
+      <Switch>
+        <Route path="/recipes" component={Recipes} />
+        <Route path="/blog" component={Blog} />
+        <Route path="/" component={About} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
