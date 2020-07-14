199e7caad9cb32be97b61fe48588a1f9b148c7eb
davidkerr766 <david.kerr766@outlook.com>
2020-07-14 16:01:41 +1000

    script added for deployment

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,25 +1,27 @@
-import React, { useState } from 'react'
-import axios from 'axios'
+import React, { useState } from "react";
+import api from "../api";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
 
-    const [blogs, setBlogs] = useState([])
+  api
+    .get("/blogs")
+    .then((res) => {
+      setBlogs(res.data);
+    });
 
-    axios.get("https://limitless-island-04616.herokuapp.com/api/blogs")
-        .then(data => {
-            setBlogs(data.data)
-        })
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {blogs.map((blog, index) => (
+          <li key={index}>
+            Title: {blog.title}. Body: {blog.body}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Blog</h1>
-            <ul>
-            { blogs.map((blog, index) => (
-                <li key={index}>Title: {blog.title}. Body: {blog.body}</li>
-            ))}
-            </ul>
-        </div>
-    )
-}
-
-export default Blog
+export default Blog;
