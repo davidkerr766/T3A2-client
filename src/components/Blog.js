422ed32a7129fcbaf46f34eeabea87222c3830e6
davidkerr766 <david.kerr766@outlook.com>
2020-07-14 14:14:56 +1000

    fetching blogs from api server on localhost

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Blog = () => {
+
+    const [blogs, setBlogs] = useState([])
+
+    axios.get("https://limitless-island-04616.herokuapp.com/api/blogs")
+        .then(data => {
+            setBlogs(data.data)
+        })
+
     return (
         <div>
             <h1>Blog</h1>
+            <ul>
+            { blogs.map((blog, index) => (
+                <li key={index}>Title: {blog.title}. Body: {blog.body}</li>
+            ))}
+            </ul>
         </div>
     )
 }
